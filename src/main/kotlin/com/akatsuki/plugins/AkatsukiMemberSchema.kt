afca024adfa8eba37fad287633ba9e7e7a44de34
Yoru-cyber <carlosmendez170210@gmail.com>
2024-03-07 14:00:21 -0400

    akatsuki service created

--- src/main/kotlin/com/akatsuki/plugins/AkatsukiMemberSchema.kt
+++ src/main/kotlin/com/akatsuki/plugins/AkatsukiMemberSchema.kt
@@ -1,25 +1,19 @@
 package com.akatsuki.plugins
+import com.mongodb.client.MongoClient
 import kotlinx.serialization.Serializable
-import org.jetbrains.exposed.sql.*
-import org.jetbrains.exposed.sql.transactions.transaction
+import org.litote.kmongo.getCollection
 
 @Serializable
 data class AkatsukiMember(val id: Int, val name: String, val alias: String, val status: String, val range: String, val description: String)
-class AkatsukiService(private val database: Database){
-    object Members : Table() {
-        val id = integer("id").autoIncrement()
-        val name = varchar("name", length = 20)
-        val alias = varchar("alias", length = 20)
-        val status = varchar("status", length = 20)
-        val range = varchar("range", length = 20)
-        val description = varchar("description", length = 100)
+//dependency injection of mongo client
+class AkatsukiService(private val client: MongoClient){
 
-        override val primaryKey = PrimaryKey(id)
-    }
-    init {
-        transaction(database) {
-            SchemaUtils.create(AkatsukiService.Members)
-        }
+    val database = client.getDatabase("akatsuki")
+    val collection = database.getCollection<AkatsukiMember>("members")
+
+
+    fun GetAll(): List<AkatsukiMember>{
+        return collection.find().toList()
     }
 
 }
