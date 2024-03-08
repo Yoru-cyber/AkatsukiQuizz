3e34ce68a00bbbca253c3b6c0d9fb25f50138785
Yoru-cyber <carlosmendez170210@gmail.com>
2024-03-08 11:49:44 -0400

    added try catch block with error handling

--- src/main/kotlin/com/akatsuki/routers/AkatsukiRouter.kt
+++ src/main/kotlin/com/akatsuki/routers/AkatsukiRouter.kt
@@ -1,16 +1,27 @@
 package com.akatsuki.routers
 
+import com.akatsuki.plugins.AkatsukiMember
 import com.akatsuki.plugins.AkatsukiService
 import com.mongodb.client.MongoClient
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.akatsukiAll(client: MongoClient){
     get("Akatsuki"){
-        val service = AkatsukiService(client)
-        val members = service.getAll()
+        val akatsukiMembers: List<AkatsukiMember>
+        try {
+            val service = AkatsukiService(client)
+            akatsukiMembers = service.getAll()
+            call.respond(status = HttpStatusCode.OK, akatsukiMembers)
+        }
+        catch (error: Throwable){
+            application.log.error("Error on getting members", error)
+            call.respond(status = HttpStatusCode.InternalServerError, "An internal error happened")
+        }
+        
 
-        call.respond(members)
+
     }
 }
