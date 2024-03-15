becd02f75049ed354c89555cf75bcbd0c0595bdc
Yoru-cyber <carlosmendez170210@gmail.com>
2024-03-15 18:26:04 -0400

    earlier return of error when question not found on array

--- AkatsukiQuizz/src/components/Question.tsx
+++ AkatsukiQuizz/src/components/Question.tsx
@@ -1,34 +1,61 @@
+import { useTranslation } from "react-i18next";
 import IQuestion from "../interfaces/IQuestion";
 
 const questionsArr: Array<IQuestion> = [
-    {
-        id: 1,
-        question: "What is your MBTI?",
-        answers: ["INFP", "INTJ", "ENFP", "ISTJ"]
-    },
-    {
-        id: 2,
-        question: "What is your favorite color?",
-        answers: ["red", "blue", "green", "yellow"]
-    },
-    {
-        id: 3,
-        question: "What is your favorite movie?",
-        answers: ["Lord of the rings", "Harry potter", "Dune", "Star Wars"]
-    }
+  {
+    id: 1,
+    question: "What is your motivation?🏁",
+    answers: [
+      "To achieve peace through world domination.",
+      "To collect and utilize powerful creatures for personal gain.",
+      "To prove your artistic merit through explosive creations.",
+      "To find a way to cheat death and live forever.",
+      "To protect your loved ones, even at a great cost.",
+    ],
+  },
+  {
+    id: 2,
+    question: "What is your favorite color?",
+    answers: ["red", "blue", "green", "yellow"],
+  },
+  {
+    id: 3,
+    question: "What is your favorite movie?",
+    answers: ["Lord of the rings", "Harry potter", "Dune", "Star Wars"],
+  },
 ];
-function Question(props: {Id: number}) {
-    const {Id} = props;
-    return (
-        <>
-            <div style={{textAlign: "center"}}>
-                <h1>This is Question</h1>
-                {Id ? <p>Question {questionsArr.find(q => q.id === Id)?.question}</p> : null} {/* it should be checked first if arr[Id] exists/*/}
-                <ul style={{listStyleType: "none", display: "flex", flexDirection: "column", gap: "10px", justifyContent: "center", alignItems: "center"}}>
-                    {questionsArr.find(q => q.id === Id)?.answers.map((answer, index) => <li key={index}><button >{answer}</button></li>)}
-                </ul>
-            </div>
-        </>
-    );
+function Question(props: { Id: number }) {
+    const {t} =useTranslation(["test"]);
+  const { Id } = props;
+  return (
+    <>
+      <div style={{ textAlign: "center", marginTop: "250px" }}>
+        {questionsArr.find((question) => question.id === Id)?(
+            <>
+              <p>{questionsArr.find((question) => question.id === Id)?.answers.map((answer) => <button>{answer}</button>)}</p>
+              </>
+        ) : <p>{t("Question.Error.NotFound")}</p>}
+        {/* it should be checked first if arr[Id] exists/*/}
+        <ul
+          style={{
+            listStyleType: "none",
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {/* {questionsArr
+            .find((q) => q.id === Id)
+            ?.answers.map((answer, index) => (
+              <li key={index}>
+                <button>{answer}</button>
+              </li>
+            ))} */}
+        </ul>
+      </div>
+    </>
+  );
 }
-export default Question
+export default Question;
