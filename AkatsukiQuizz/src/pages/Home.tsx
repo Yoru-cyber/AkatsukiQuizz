550bb544c4d9e4a92390712cf1e4134df554e3df
Yoru-cyber <carlosmendez170210@gmail.com>
2024-03-24 20:25:37 -0400

    Refactored Home Component to tailwind support

--- AkatsukiQuizz/src/pages/Home.tsx
+++ AkatsukiQuizz/src/pages/Home.tsx
@@ -2,62 +2,48 @@ import NavBar from "../components/NavBar";
 import styled from "styled-components";
 import { useTranslation } from "react-i18next";
 import { Suspense } from "react";
-const Title = styled.h1`
-  font-family: "Sedgwick Ave Display";
-  color: var(--primary-1);
 
-`;
-const FlexContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-  width: 100vw;
-  height: 100vh;
-  background-image: url("src/assets/background.webp");
-  background-size: cover;
-  background-position: center;`
-const ImgContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 40%;
-  margin-top: 10px;`
 const Button = styled.button`
-border-radius: 5px;
-border: 2px solid var(--primary-1);
-background-color: var(--background-color);
-color: var(--foreground-color);
-padding: 10px 20px;
-margin-top: 20px;
-transition: transform 0.2s ease-in-out;
-font-family: 'Inter', sans-serif;
-font-weight: bold;
-&:hover {
-  cursor: pointer;
-  transform: scale(1.1);
-}
+  border-radius: 5px;
+  border: 2px solid var(--primary-1);
+  background-color: var(--background-color);
+  color: var(--foreground-color);
+  padding: 10px 20px;
+  margin-top: 20px;
+  transition: transform 0.2s ease-in-out;
+  font-family: "Inter", sans-serif;
+  font-weight: bold;
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.1);
+  }
 `;
-
 
 function Home() {
-  const {t} = useTranslation(["test"]);
+  const { t } = useTranslation(["test"]);
 
   return (
     <>
-    <Suspense>
-    
-      <FlexContainer>
-      <NavBar />
-      <Title>Akatsuki Quizz</Title>
-      <ImgContainer>
-      <img style={{marginTop: "10px"}} src="src/assets/orochimaru-chibi.webp"></img>
-      </ImgContainer>
-      <h2 style={{fontWeight: "500", textShadow: "2px 2px 10px black"}}>{t("Home.SecondTitle")}</h2>
-      <Button onClick={() => window.location.href = "/quizz"}>{t("Home.Quizz")}</Button>
-      </FlexContainer>
+      <Suspense>
+        <main className="flex flex-col items-center w-svw h-svh bg-[url('src/assets/background.webp')] bg-cover bg-center">
+          <NavBar />
+          <h1
+            className="text-3xl text-red-700"
+            style={{ fontFamily: "Sedgwick Ave Display" }}
+          >
+            Akatsuki Quizz
+          </h1>
+          <img className="" src="src/assets/orochimaru-chibi.webp"></img>
+          <h2 style={{ fontWeight: "500", textShadow: "2px 2px 10px black" }}>
+            {t("Home.SecondTitle")}
+          </h2>
+          <button
+            className="h-10 w-36 rounded-lg text-white font-bold bg-black border-red-700 border-2 hover:animate-pulse"
+            onClick={() => (window.location.href = "/quizz")}
+          >
+            {t("Home.Quizz")}
+          </button>
+        </main>
       </Suspense>
     </>
   );
